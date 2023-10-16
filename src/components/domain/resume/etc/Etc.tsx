affe38b2f6f00247f3c4dc68a84a5fb732a6a9e7
cherryc0ck <qstar9537@naver.com>
2023-10-16 23:14:55 +0900

    :adhesive_bandage: Etc 텍스트 수정

--- src/components/domain/resume/etc/Etc.tsx
+++ src/components/domain/resume/etc/Etc.tsx
@@ -4,7 +4,7 @@ import * as S from "./styled";
 
 const DATA = [
   {
-    name: "프론트엔드 질문&외주 오픈채팅방 운영",
+    name: "프론트엔드 질문&멘토 오픈채팅방 운영",
     date: "2023.03 ~ 현재",
     link: "https://open.kakao.com/o/soKSAV3c",
   },
